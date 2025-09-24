b406059f2893ca4430111e92f7f2ae49404515c0
LukeFrankio <frankioluke@gmail.com>
2025-09-24 12:35:31 -0300

    smoler and iterative

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-int f(int n){return n<2?n:f(n-1)+f(n-2);}int main(int c,char**v){int n=c<2?10:0;for(char*p=v[1];*p;p++)n=n*10+*p-48;std::cout<<f(n);} 
+int main(int c,char**v){int n=c<2?10:0;for(char*p=v[1];*p;p++)n=n*10+*p-48;int a=0,b=1;while(n--)b+=a,a=b-a;std::cout<<a;} 
