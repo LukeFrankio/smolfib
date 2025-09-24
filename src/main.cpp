6f9d631c6f54773345c96cdb75162c56eede5aae
LukeFrankio <frankioluke@gmail.com>
2025-09-24 12:29:27 -0300

    made smoler

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,2 @@
-#include <iostream>
-long long f(long long n){return n<2?n:f(n-1)+f(n-2);} // naive recursive (few lines)
-int main(int argc,char** argv){long long n= (argc>1? std::atoll(argv[1]):10);std::cout<<f(n)<<"\n";}
+#include<iostream>
+int f(int n){return n<2?n:f(n-1)+f(n-2);}int main(int c,char**v){int n=10;if(c>1){n=0;for(char*p=v[1];*p;p++)n=n*10+*p-48;}std::cout<<f(n);} 
