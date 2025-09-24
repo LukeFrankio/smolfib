dff99b76d8a33500b394a2b55829a33d8b15c174
LukeFrankio <frankioluke@gmail.com>
2025-09-24 12:34:33 -0300

    smooooler

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-int f(int n){return n<2?n:f(n-1)+f(n-2);}int main(int c,char**v){int n=10;if(c>1){n=0;for(char*p=v[1];*p;p++)n=n*10+*p-48;}std::cout<<f(n);} 
+int f(int n){return n<2?n:f(n-1)+f(n-2);}int main(int c,char**v){int n=c<2?10:0;for(char*p=v[1];*p;p++)n=n*10+*p-48;std::cout<<f(n);} 
